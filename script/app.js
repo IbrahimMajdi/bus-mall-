227c00bfd26a1c7cabe83bdddbaac80000d8beb8
IbrahimMajdi <ibrmaj1@gmail.com>
2020-06-06 13:31:03 +0300

    added more css and local storage fixes

--- script/app.js
+++ script/app.js
@@ -1,18 +1,23 @@
 'use static'
 
+// get the uplaoded images
 var inputfolder = document.getElementById("file-selector").files;
+
+// get the  images html tags
 
 var leftImage = document.getElementById("left");
 var centerImage = document.getElementById("center");
 var rightImage = document.getElementById("right");
 var imageSection = document.getElementById("section");
 
+// declare and assign global variables
 var totalClicks = 0;
 var voteRounds = 25;
 Products.all = [];
 var imagesSet = [];
 
 
+// constructor function ( contain the alt and the src of the img )
 
 function Products(name) {
 
@@ -26,20 +31,27 @@ function Products(name) {
 }
 
 
+// iterate through objects to creat them
 
 for (var i = 0; i < inputfolder.length; i++) {
 
     new Products(inputfolder[i].name)
 }
 
+// declare the variables to save the random images
+
 var left, center, right;
+
+// function to render random images
 
 function renderImages() {
 
+    // generate 3 random images
     left = randGenerator();
     center = randGenerator();
     right = randGenerator();
 
+    // make sure there are no tow images are similar on the same round, also no tow rounds have similar images.
 
     if (left === center || center === right || right === left || imagesSet.includes(left) || imagesSet.includes(center) || imagesSet.includes(right)) {
 
@@ -49,12 +61,12 @@ function renderImages() {
     } else {
 
         imagesSet = [];
-        // console.log("else imagesSet", imagesSet);
     }
 
-
+    // push the generated images to an array to compare
     imagesSet.push(left, center, right);
 
+    // assign images src and alt while incrementing image appearance count 
 
     leftImage.src = left.imgpath;
     leftImage.title = left.productname;
@@ -70,11 +82,12 @@ function renderImages() {
     right.view++
 }
 
+// render images at first page lunch
 renderImages();
 
+// add event listner for user clicks
+
 imageSection.addEventListener('click', handler);
-
-
 
 function handler(event) {
 
@@ -98,12 +111,12 @@ function handler(event) {
             if (event.target.id === 'right') {
                 right.clicks++;
             }
-
             SetProducts();
+
             renderImages();
         }
 
-
+        // check if the current totalcliks is equal to the fixed voteRounds number to display the final data
 
     } else if (totalClicks === voteRounds - 1) {
         imageSection.removeEventListener('click', handler);
@@ -113,7 +126,7 @@ function handler(event) {
 
 }
 
-
+// render the final data list with a chart for the data
 
 function finalResult() {
 
@@ -128,21 +141,24 @@ function finalResult() {
     chart();
 }
 
-
+// generaate random product images
 
 function randGenerator() {
     return Products.all[randomNum(0, inputfolder.length - 1)];
 }
 
+// helper function to generate random numbers
 function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//  arrays to use to fill the chart data
 var productsLables = [];
 var productClicks = [];
 var productViews = [];
 
 
+// chart js function
 
 function chart() {
 
@@ -185,18 +201,23 @@ function chart() {
     });
 }
 
+// set local storage
 function SetProducts() {
 
     var data = JSON.stringify(Products.all);
     localStorage.setItem('data', data);
 
 }
+// get local storage
 
 function getProducts() {
 
     var storedData = localStorage.getItem('data');
-    Products.all = JSON.parse(storedData)
-    console.log("parsedData", Products.all);
-    finalResult();
+    if (storedData != null) {
+
+        Products.all = JSON.parse(storedData);
+        finalResult();
+    }
 }
+// retrive local storage data
 getProducts();
